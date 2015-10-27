1ba2b3b5339d0e8746446544c4ddc9db69266bb9
Timur Zaripov <tzaripov@iponweb.net>
2015-10-27 22:19:26 +0300

    added basic commanline interface

--- src/lantern/app.py
+++ src/lantern/app.py
@@ -1,13 +1,26 @@
 # coding: utf-8
 import struct
+import optparse
 from multiprocessing import Process
 
-from config import HOST, PORT
+from config import HOST, PORT, DEFAULT_POWERED, DEFAULT_COLOR
 from client import TLVClient
 from lantern import Lantern
 from log import create_logger
 
 logger = create_logger(__name__)
+
+
+parser = optparse.OptionParser()
+parser.add_option("-c", "--color", type="string",
+                  default=DEFAULT_COLOR,
+                  help="initial lantern color")
+parser.add_option("-p", "--powered",
+                  action="store_true", default=DEFAULT_POWERED,
+                  help="initial lantern power state")
+parser.add_option("--console",
+                  action="store_true", default=False,
+                  help="use console lantern")
 
 
 def start_client(lantern):
@@ -43,13 +56,12 @@ def start_client(lantern):
 
 if __name__ == '__main__':
     logger.debug('starting app')
-    console = True
+    (options, args) = parser.parse_args()
 
-    # message queue for communication between Lantern object and UI
-    #lantern = Lantern(powered=options.powered, color=options.color)
-    lantern = Lantern()
+    # Lantern object with message queue for communication with UI
+    lantern = Lantern(powered=options.powered, color=options.color)
 
-    if not console:
+    if not options.console:
         # importing ui here to allow app run without any graphic environment at all
         from ui import DCGreenLantern
         lantern_ui = DCGreenLantern(message_q=lantern.changes_q)
